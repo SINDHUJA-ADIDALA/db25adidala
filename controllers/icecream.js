4de6c8eed704e07bcac2723caf2496276ee14c04
SINDHUJA-ADIDALA <89418465+SINDHUJA-ADIDALA@users.noreply.github.com>
2021-11-17 19:05:40 -0600

    updated info

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -45,14 +45,36 @@ exports.icecream_create_post = async function(req, res) {
     }   
 }; 
  
-// Handle icecream delete form on DELETE. 
-exports.icecream_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id); 
+// Handle icecream delete on DELETE. 
+exports.icecream_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await icecream.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
- 
+
 // Handle icecream update form on PUT. 
-exports.icecream_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream update PUT' + req.params.id); 
+exports.icecream_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await icecream.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.flavour)  
+               toUpdate.flavour = req.body.flavour; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        if(req.body.toppings) toUpdate.toppings = req.body.toppings; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 
 
 // VIEWS 
@@ -67,3 +89,59 @@ exports.icecream_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+//Handle a show one view with id specified by query 
+exports.icecream_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await icecream.findById( req.query.id) 
+        res.render('icecreamdetail',  
+{ title: 'icecream Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
+// Handle building the view for creating a icecream. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.icecream_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('icecreamcreate', { title: 'icecream Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a icecream. 
+// query provides the id 
+exports.icecream_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await icecream.findById(req.query.id) 
+        res.render('icecreamupdate', { title: 'icecream Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.icecream_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await icecream.findById(req.query.id) 
+        res.render('icecreamdelete', { title: 'icecream Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/icecream.js
+++ routes/icecream.js
@@ -5,4 +5,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 
+/* GET detail icecream page */ 
+router.get('/detail', icecream_controlers.icecream_view_one_Page); 
+ 
+/* GET create icecream page */ 
+router.get('/create', icecream_controlers.icecream_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', icecream_controlers.icecream_update_Page); 
+
+/* GET create icecream page */
+router.get('/delete', icecream_controlers.icecream_delete_Page);
 module.exports = router;
